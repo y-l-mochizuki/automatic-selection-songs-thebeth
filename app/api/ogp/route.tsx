29b97aab2b61a9709954cba3324b7529f796e072
y-l-mochizuki <mczk9402@gmail.com>
2024-03-08 23:10:41 +0900

    add: ogp

---
+++ app/api/ogp/route.tsx
@@ -0,0 +1,74 @@
+import { ImageResponse } from "@vercel/og";
+import { NextRequest } from "next/server";
+
+export const config = {
+  runtime: "edge",
+};
+
+export async function GET(req: NextRequest) {
+  const url = new URL(req.url);
+  const searchParams = new URLSearchParams(url.search);
+  const title = searchParams.get("title") || "";
+  const data = await fetchFont(title);
+  if (!data) {
+    return new Response(null, {
+      status: 404,
+    });
+  }
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 40,
+          color: "black",
+          background: "white",
+          width: "100%",
+          height: "100%",
+          padding: "50px 200px",
+          textAlign: "center",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {title}
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+      fonts: [
+        {
+          name: "NotoSansJP",
+          style: "normal",
+          data,
+        },
+      ],
+    },
+  );
+}
+
+export async function fetchFont(text: string): Promise<ArrayBuffer | null> {
+  // NOTE: Noto Sans JP を使用する場合は以下の URL になる。（他のフォントを使用する場合は別のフォント名にする）
+  // text パラメータを付与することで、使用する文字のみをサブセット化を行い通信容量を削減しながら通信が可能となる。
+  const googleFontsUrl = `https://fonts.googleapis.com/css2?family=Noto+Sans+JP&text=${encodeURIComponent(
+    text,
+  )}`;
+
+  const css = await (
+    await fetch(googleFontsUrl, {
+      headers: {
+        "User-Agent":
+          "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; de-at) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1",
+      },
+    })
+  ).text();
+
+  const resource = css.match(
+    /src: url\((.+)\) format\('(opentype|truetype)'\)/,
+  );
+
+  if (!resource) return null;
+  const res = await fetch(resource[1]);
+  return res.arrayBuffer();
+}
---
+++ app/ogp copy/route.tsx
@@ -0,0 +1,43 @@
+import { ImageResponse } from "@vercel/og";
+import { url } from "inspector";
+import { NextRequest } from "next/server";
+
+export const config = {
+  runtime: "edge",
+};
+
+export async function GET(req: NextRequest) {
+  // NOTE: テキストをパラメータで指定することで動的な文章の表示が行える
+  // URLオブジェクトを作成します。
+  const url = new URL(req.url);
+
+  // URLの検索部分（クエリ文字列）からURLSearchParamsオブジェクトを作成します。
+  const searchParams = new URLSearchParams(url.search);
+
+  // 特定のパラメータを取得します。例えば 'result' パラメータ
+  const result = searchParams.get("result");
+
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 40,
+          color: "black",
+          background: "white",
+          width: "100%",
+          height: "100%",
+          padding: "50px 200px",
+          textAlign: "center",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        {result}
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630,
+    },
+  );
+}
